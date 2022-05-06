539952c5a1943a38bab8d202273be5ba2228be18
dylandupasquier <dylandupasquier@gmail.com>
2022-05-06 16:16:57 -0700

    added basic bg

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,10 +2,12 @@ import '../sass/style.css';
 import Navigation from './Navigation';
 import Container from './Container';
 import Footer from './Footer';
+import ParticlesBg from './ParticlesBg.jsx';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <ParticlesBg />
       <Navigation />
       <Container />
       <Footer />
