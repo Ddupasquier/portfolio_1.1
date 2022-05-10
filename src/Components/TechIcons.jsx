5a7c30f942d8f7ce3ea0f8e0503b44487091898f
dylandupasquier <dylandupasquier@gmail.com>
2022-05-10 14:58:31 -0700

    title icons

--- src/Components/TechIcons.jsx
+++ src/Components/TechIcons.jsx
@@ -4,20 +4,19 @@ import * as DiIcon from 'react-icons/di';
 function TechIcons() {
   return (
     <>
-      <DiIcon.DiReact />
-      <DiIcon.DiGithubBadge />
-      <DiIcon.DiCss3 />
-      <DiIcon.DiSass />
-      <DiIcon.DiHtml5 />
-      <DiIcon.DiJsBadge />
-      <DiIcon.DiLinux />
-      <DiIcon.DiUbuntu />
-      <DiIcon.DiVisualstudio />
-      <DiIcon.DiHeroku />
-      <DiIcon.DiPostgresql />
-      <DiIcon.DiRuby />
-      <DiIcon.DiUbuntu />
-      <DiIcon.DiStackoverflow />
+      <DiIcon.DiReact title="React" />
+      <DiIcon.DiGithubBadge title="GitHub" />
+      <DiIcon.DiCss3 title="CSS3" />
+      <DiIcon.DiSass title="SCSS/Sass" />
+      <DiIcon.DiHtml5 title="HTML5" />
+      <DiIcon.DiJsBadge title="JavaScript" />
+      <DiIcon.DiLinux title="Linux" />
+      <DiIcon.DiUbuntu title="Ubuntu" />
+      <DiIcon.DiVisualstudio title="VSCode" />
+      <DiIcon.DiHeroku title="Heroku" />
+      <DiIcon.DiPostgresql title="PostgreSQL" />
+      <DiIcon.DiRuby title="Ruby on Rails" />
+      <DiIcon.DiStackoverflow title="StackOverflow" />
     </>
   );
 }
