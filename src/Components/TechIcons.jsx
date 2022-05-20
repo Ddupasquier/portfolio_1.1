544978db86183513c1864deb5a89599d71579b0d
dylandupasquier <dylandupasquier@gmail.com>
2022-05-19 17:37:02 -0700

    add asset index

--- src/Components/TechIcons.jsx
+++ src/Components/TechIcons.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-// import * as DiIcon from 'react-icons/di';
-import reactIcon from '../assets/tech-svg/react-original-wordmark.svg';
-import cssIcon from '../assets/tech-svg/css3-original-wordmark.svg';
-import scssIcon from '../assets/tech-svg/sass-original.svg';
-import jsIcon from '../assets/tech-svg/javascript-original.svg';
-import htmlIcon from '../assets/tech-svg/html5-original-wordmark.svg';
-import linuxIcon from '../assets/tech-svg/linux-original.svg';
-import railsIcon from '../assets/tech-svg/rails-original-wordmark.svg';
-import rubyIcon from '../assets/tech-svg/ruby-original.svg';
-import postgresIcon from '../assets/tech-svg/postgresql-original-wordmark.svg';
-import stackIcon from '../assets/tech-svg/512px-Stack_Overflow_icon.svg.png';
-import vsIcon from '../assets/tech-svg/visual-studio-code.svg';
-import ubuntuIcon from '../assets/tech-svg/ubuntu.svg';
-import gitIcon from '../assets/tech-svg/iconmonstr-github-1-240.png';
-import netlifyIcon from '../assets/tech-svg/netlify.svg';
+import {
+  reactIcon,
+  cssIcon,
+  scssIcon,
+  jsIcon,
+  htmlIcon,
+  linuxIcon,
+  railsIcon,
+  rubyIcon,
+  postgresIcon,
+  stackIcon,
+  vsIcon,
+  ubuntuIcon,
+  gitIcon,
+  netlifyIcon,
+} from '../assets/index';
 
 function TechIcons() {
   const icons = [
