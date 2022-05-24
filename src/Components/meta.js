a108c78db5ced070bdc717aaf588cd050a9cfdcb
dylandupasquier <dylandupasquier@gmail.com>
2022-05-23 19:15:41 -0700

    test

--- src/Components/meta.js
+++ src/Components/meta.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import Preview from '../assets/Untitled.jpg';
+// import Preview from '../assets/Untitled.jpg';
 
 function Meta() {
   return (
