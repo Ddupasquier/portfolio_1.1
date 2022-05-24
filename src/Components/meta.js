621214a5df6daafd815ac965ec40e85460fb5928
dylandupasquier <dylandupasquier@gmail.com>
2022-05-23 19:25:17 -0700

    test

--- src/Components/meta.js
+++ src/Components/meta.js
@@ -4,16 +4,18 @@ import { Helmet } from 'react-helmet';
 
 function Meta() {
   return (
-    <Helmet>
-      <meta charSet="utf-8" />
-      <title>Dupasquier.dev</title>
-      <meta name="og:title" content="Dupasquier.dev" />
-      <meta
-        name="og:description"
-        content="This portfolio is where I will be displaying projects as well as providing contact information to perspective employers."
-      />
-      <link rel="canonical" href="https://www.dupasquier.dev" />
-    </Helmet>
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Dupasquier.dev</title>
+        <meta property="og:title" content="Dupasquier.dev" />
+        <meta
+          property="og:description"
+          content="This portfolio is where I will be displaying projects as well as providing contact information to perspective employers."
+        />
+        <link rel="canonical" href="https://www.dupasquier.dev" />
+      </Helmet>
+    </div>
   );
 }
 
